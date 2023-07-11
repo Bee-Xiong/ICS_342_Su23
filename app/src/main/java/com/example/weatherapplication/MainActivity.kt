5b102698e8657a8c8847f1ff4a7b8cc48309207d
Bee-Xiong <bxiong1990@gmail.com>
2023-07-10 21:11:27 -0500

    Assigment 2

--- app/src/main/java/com/example/weatherapplication/MainActivity.kt
+++ app/src/main/java/com/example/weatherapplication/MainActivity.kt
@@ -1,20 +1,21 @@
 package com.example.weatherapplication
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
@@ -22,34 +23,45 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.weatherapplication.ui.theme.WeatherApplicationTheme
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
 
 class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Surface(
-                modifier = Modifier.fillMaxSize(),
-                color = MaterialTheme.colorScheme.background
-            ) {
-                MyApp()
+            val navController = rememberNavController()
+            NavHost(navController = navController, startDestination = "Start") {
+                this.composable("Start") {
+                    MyApp(navController)
+                }
+                composable(
+                    "ForecastScreen",
+                ) {
+                    ForecastScreen(ForecastItems)
+                }
             }
         }
     }
 }
 
+@RequiresApi(Build.VERSION_CODES.O)
 @Composable
-fun MyApp() {
+fun MyApp(navController: NavController) {
     Column(
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        TitleBar()
+        TitleBar(stringResource(R.string.app_name))
         Location()
         Row(
             verticalAlignment = Alignment.CenterVertically
@@ -60,7 +72,7 @@ fun MyApp() {
                 CurrentTemp()
             }
             Spacer(modifier = Modifier.width(width = 75.dp))
-            MyImage()
+            MyImage(modifier = Modifier.size(100.dp))
         }
         Column(
             modifier = Modifier
@@ -69,15 +81,18 @@ fun MyApp() {
         ) {
             MoreInfo()
         }
+        Column {
+            ForecastButton(navController)
+        }
     }
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun TitleBar() {
+fun TitleBar(text: String) {
     TopAppBar(
         title = {
-            Text(text = stringResource(R.string.app_name))
+            Text(text = text)
         },
         colors = TopAppBarDefaults.smallTopAppBarColors(containerColor = Color.Gray)
     )
@@ -85,66 +100,58 @@ fun TitleBar() {
 
 @Composable
 fun Location() {
-    Text(
-        text = stringResource(R.string.location),
-        style = TextStyle(
-            fontSize = 20.sp,
-        )
-    )
+    Texts(stringResource(R.string.location), 20.sp)
 }
 
 @Composable
 fun CurrentTemp() {
-    Text(
-        text = stringResource(R.string.currTemp),
-        style = TextStyle(
-            fontSize = 75.sp,
-        )
-    )
-    Text(
-        text = stringResource(R.string.feel),
-    )
+    val currInfo = ForecastItems[0]
+    val currTemp = stringResource(R.string.currTemp, currInfo.temp.day.toInt())
+    Texts(currTemp,75.sp)
+    Texts(stringResource(R.string.feel), 12.sp)
 }
 
 @Composable
-fun MyImage() {
+fun MyImage(modifier: Modifier = Modifier) {
     Image(
         painter = painterResource(id = R.drawable._1d),
         contentDescription = stringResource(R.string.image_description),
-        modifier = Modifier.size(100.dp)
+        modifier = modifier
     )
 }
 
+@RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun MoreInfo() {
+    val currInfo = ForecastItems[0]
+    Texts(stringResource(R.string.low, currInfo.temp.min.toInt()), 20.sp)
+    Texts(stringResource(R.string.high, currInfo.temp.max.toInt()), 20.sp)
+    Texts(stringResource(R.string.humidity, currInfo.humidity), 20.sp)
+    Texts(stringResource(R.string.pressure, currInfo.pressure.toInt()), 20.sp)
+}
+
+@Composable
+fun Texts(text: String, size: TextUnit) {
     Text(
-        text = stringResource(R.string.low),
-        style = TextStyle(
-            fontSize = 20.sp,
-        )
-    )
-    Text(
-        text = stringResource(R.string.high),
-        style = TextStyle(
-            fontSize = 20.sp,
-        )
-    )
-    Text(
-        text = stringResource(R.string.humidity),
-        style = TextStyle(
-            fontSize = 20.sp,
-        )
-    )
-    Text(
-        text = stringResource(R.string.pressure),
-        style = TextStyle(
-            fontSize = 20.sp,
-        )
+        text = text,
+        fontSize = size
     )
 }
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    MyApp()
+fun ForecastButton(navController: NavController) {
+    Button(
+        onClick = { navController.navigate("ForecastScreen") },
+        modifier = Modifier.width(150.dp),
+        colors = ButtonDefaults.buttonColors(Color.Gray),
+        shape = RectangleShape,
+    ) {
+        Text(text = "Forecast", color = Color.Black)
+    }
 }
+
+//@Preview(showBackground = true)
+//@Composable
+//fun DefaultPreview() {
+//    MyApp()
+//}
